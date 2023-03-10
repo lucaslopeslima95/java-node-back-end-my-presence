247516a82032159e06009251780c105f14ee4e4a
lucaslopeslima95 <lucaslopesllima@gmail.com>
2023-03-10 20:10:56 -0300

    update logical auth in user controller

--- src/main/java/com/minhapresenca/minhapresencabackend/controller/UserController.java
+++ src/main/java/com/minhapresenca/minhapresencabackend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.minhapresenca.minhapresencabackend.controller;
 
 import com.minhapresenca.minhapresencabackend.DTO.UserDTO;
 import com.minhapresenca.minhapresencabackend.servicesImplementations.UserServiceImpl;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,11 +17,11 @@ public class UserController {
   public UserController(UserServiceImpl userService) {
     this.userService = userService;
   }
-
+//if (BCrypt.checkpw(candidate_password, stored_hash))
 
   @PostMapping
   public Boolean logIn(@RequestBody UserDTO userDTO) {
-     Optional<Boolean> aBoolean = Optional.of(userService.findByEmailAndPassword(userDTO.email(), userDTO.password()));
+     Optional<Boolean> aBoolean = Optional.of(userService.findByEmailAndPassword(userDTO.email(), BCrypt.checkpw(candidate_password, stored_hash)));
       if (aBoolean.isPresent()){
         return true;
       }
